f5cda55057d60701bdf5bc472308726437332308
wb.yexulong <wb.yexulong@mesg.corp.netease.com>
2020-11-26 10:54:41 +0800

    上传django-rest项目

---
+++ api/serializers.py
@@ -0,0 +1,29 @@
+#!C:\Users\wb.yexulong\AppData\Local\Programs\Python\Python37\python.exe
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from api.models import Event, Guest
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('url', 'name', 'address', 'start_time', 'limit', 'status')
+
+
+class GuestSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Guest
+        fields = ('url', 'realname', 'phone', 'email', 'sign', 'event')
---
+++ api/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from api.serializers import UserSerializer, GroupSerializer, EventSerializer, GuestSerializer
+from api.models import Event, Guest
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class GuestViewSet(viewsets.ModelViewSet):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
